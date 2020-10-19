aaad5a1d3b84cbb98a881513a977464be745eece
NELEonce <neleonce.999@gmail.com>
2020-10-19 10:17:36 +0900

    Scene遷移

---
+++ Don't stop!!/Assets/TestNele/Script/MouseFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private 
+ 
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 10.0f;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        this.transform.position = new Vector3(mousePos.x+0.3f, mousePos.y-0.3f, mousePos.z);
+
+        /*
+        // 一つ一つの位置を取得
+        ParticleSystem.Particle[] mps = new ParticleSystem.Particle[ps.emission.burstCount];
+        ps.GetParticles(mps);
+        foreach (var p in mps)
+        {
+            if ()
+            {
+
+            }
+        }
+        */
+    }
+}
---
+++ Don't stop!!/Assets/TestNele/Script/test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Doozy.Engine.UI;
+
+public class test : MonoBehaviour
+{
+    [SerializeField]
+    string PopupName;
+
+    public void PopupShow()
+    {
+        UIPopup popup = UIPopupManager.GetPopup(PopupName);
+        UIPopupManager.ShowPopup(popup, popup.AddToPopupQueue, false);
+    }
+}
