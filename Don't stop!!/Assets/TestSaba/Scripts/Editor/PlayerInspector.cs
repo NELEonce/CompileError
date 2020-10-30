b17eb5173b7dde80f7c0a161638ef7d9469b0004
verysabakan <full_bloom@au.com>
2020-10-30 12:20:06 +0900

    saba入れ直し

---
+++ Don't stop!!/Assets/TestSaba/Scripts/Editor/PlayerInspector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public sealed class PlayerInspector : Editor
+{
+    private SerializedProperty characterHeight;
+
+    private void OnEnable()
+    {
+        characterHeight = serializedObject.FindProperty("characterHeight");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUI.indentLevel = 0;
+        //EditorUtilities.FoldoutHeader("Character Settings", characterHeight);
+
+        EditorGUI.indentLevel = 1;
+        
+
+        EditorGUILayout.PropertyField(characterHeight, new UnityEngine.GUIContent("Height (m)"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Don't stop!!/Assets/TestSaba/Scripts/hand.cs
+++ Don't stop!!/Assets/TestSaba/Scripts/hand.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class hand : MonoBehaviour
 {
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.Play("Fire", 0, 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.Play("Reload", 0, 0.0f);
+        }
     }
 }
