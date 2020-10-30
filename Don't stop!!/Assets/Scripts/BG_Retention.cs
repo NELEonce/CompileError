c38228543f00080d755f03625bb27ba913f131a6
NELEonce <neleonce.999@gmail.com>
2020-10-30 13:08:51 +0900

    変更点の修正

---
+++ Don't stop!!/Assets/Scripts/BG_Retention.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class BG_Retention : MonoBehaviour
+{
+
+    private Canvas canvas;
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    private void SceneLoaded(Scene nextScene, LoadSceneMode arg1)
+    {
+        //Practice画面に遷移したら　BGオブジェクトを壊す
+        if (nextScene.name == "Practice") Destroy(gameObject);
+
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        if(mainCamera!=null)
+        canvas.worldCamera = mainCamera.GetComponent<Camera>();
+    }
+}
---
+++ Don't stop!!/Assets/Scripts/SelectManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    private void SceneLoaded(Scene SelectScene, LoadSceneMode arg1)
+    {
+        GameObject BG_Canvas = GameObject.Find("BG_Canvas");
+        //Select画面に遷移したら　BGオブジェクトを生成する
+        if (SelectScene.name == "Select")
+        {
+            Instantiate(BG_Canvas);
+        }
+    }
+}
