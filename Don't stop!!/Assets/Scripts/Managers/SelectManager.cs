9467600eaad0891d2a744d20df6d83cf687043d9
verysabakan <full_bloom@au.com>
2020-12-21 22:48:57 +0900

    ﾏﾈｰｼﾞｬｰがはいってねぇだ

---
+++ Don't stop!!/Assets/Scripts/Managers/SelectManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;     
+
+public class SelectManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject BG_Canvas;
+
+    private void Awake()
+    {
+        GameObject.Find("ScoutfliesCanvas").GetComponent<MouseFollow>().mouseFlag = true;
+    }
+
+    void Start()
+    {
+        Time.timeScale = 1;
+        SceneManager.sceneLoaded += SceneLoaded;
+    }
+
+    private void SceneLoaded(Scene SelectScene, LoadSceneMode arg1)
+    {
+        //ｾﾚｸﾄｼｰﾝに遷移したら生成する
+        if (SelectScene.name == "Select")
+        {
+            SceneManager.sceneLoaded -= SceneLoaded;
+            GameObject mainCamera = GameObject.Find("Main Camera");
+            //背景ｵﾌﾞｼﾞｪｸﾄが無かったら生成する
+            if (mainCamera != null)
+                BG_Canvas.GetComponent<Canvas>().worldCamera = mainCamera.GetComponent<Camera>();
+                Instantiate(BG_Canvas);
+        }
+    }
+}
